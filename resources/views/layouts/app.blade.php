8024c198c91f8b6a931224a2b87b7c4aaee6c526
Michael-Kamau <kamau.karitu@gmail.com>
2021-05-10 16:26:36 +0300

    Add terms and conditions to footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -108,7 +108,9 @@
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
                                             <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
                                         </svg>
-                    <span class="ml-3 text-l">AllEventsFinder © 2021</span>
+                    <span class="ml-3 text-l">AllEventsFinder © 2021 | </span>
+                    <span>                    <p> <a href="/terms" class="text-blue-600">| Terms and conditions</a> </p>
+</span>
                 </a>
                 <p class="text-sm text-gray-500 sm:ml-6 sm:mt-0 mt-4"><i class="fa fa-envelope-open" aria-hidden="true"></i> : Antonykatei11@gmail.com <i class="fa fa-phone" aria-hidden="true"></i>
                     : 0700024779
